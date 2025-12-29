ef079a54414a3494c2c608cac1e3ed0ba344aeb8
Pranav Shah <168293709+Pusparaj99op@users.noreply.github.com>
2025-12-29 17:15:51 +0530

    practical 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Java Programming Sem-4
- in this repo Pranay is going to save the code of java which will thought in college
+# Practical 1 (Date: 29-12-2025)
+##Aims:
+##Theory: 
---
+++ pratical1/Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Calculator{
+    public static void main(String []agrs)
+    {   Scanner scanner = new Scanner(System.in);
+        System.out.println("Pranay is trying to make a Calculator!!");
+        System.out.println("========JAVA CALCULATOR INTERFACE========");
+        System.out.println("Enter First Digit");
+        int FirstDigit = scanner.nextInt();
+        System.out.println("Enter Second Digit");
+        int SecondDigit = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Please Broh!! Enter YOur Name");
+        String Name = scanner.nextLine();
+        int  sum = FirstDigit + SecondDigit;
+        System.out.println("hellow, " + Name + " the Sum of "  + FirstDigit + " And " + SecondDigit + " is " + sum);
+        scanner.close();
+    }
+}
